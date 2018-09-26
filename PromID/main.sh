8ba719a07e301ff24ccf4e030bb51b57940ed91c
root <root@kw12508.cbrc.kaust.edu.sa>
2018-09-27 00:31:09 +0300

    i

--- PromID/main.sh
+++ PromID/main.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -i
 find PromID/files -mtime +5 -type f -delete
-python PromID/pipeline/predictor_padding.py PromID/model_1 $1 251 1 PromID/files/output.txt
-python PromID/pipeline/choose_prom.py PromID/files/output.txt 
+b=$(basename $1)
+python PromID/pipeline/predictor_padding.py PromID/model_1 $1 600 1 PromID/files/${b}_output.txt
+python PromID/pipeline/choose_prom.py PromID/files/${b}_output.txt 
